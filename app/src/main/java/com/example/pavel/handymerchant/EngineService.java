e285ab351fb61aa7a7390637b52165856661791d
Pavel Titov <pavel_titov@mail.ru>
2018-02-18 23:08:58 +0300

    moved networking to service

--- app/src/main/java/com/example/pavel/handymerchant/EngineService.java
+++ app/src/main/java/com/example/pavel/handymerchant/EngineService.java
@@ -4,15 +4,78 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * Created by pavel on 13.02.18.
  */
 
 public class EngineService extends Service {
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                ChartData[] chartData = new ChartData[0];
+
+                OkHttpClient client = new OkHttpClient();
+                long todayUTC = new Date().getTime()/1000;
+                Request request = new Request.Builder().url(
+                        Util.buildUrl(Util.CurrencyPair.USDT_BTC,
+                                todayUTC - 60*60*24*5,
+                                todayUTC,
+                                Util.Period.TWO_HOURS
+                        )).build();
+                GsonBuilder builder = new GsonBuilder();
+                Gson gson = builder.create();
+
+                try (Response response = client.newCall(request).execute()) {
+                    chartData = gson.fromJson(response.body().string(), ChartData[].class);
+                    // Ограничение на количество запросов - не более 5 в секунду
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+
+                DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm", Locale.getDefault());
+                for (ChartData entry: chartData)
+                    Log.d("happy", dateFormat.format(new java.util.Date(entry.getDate()*1000)) + " : " + entry.getClose() + "\n");
+            }
+        };
+        thread.setPriority(Thread.MIN_PRIORITY);
+        thread.start();
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
     }
 }
--- app/src/main/java/com/example/pavel/handymerchant/MainActivity.java
+++ app/src/main/java/com/example/pavel/handymerchant/MainActivity.java
@@ -1,32 +1,18 @@
 package com.example.pavel.handymerchant;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 
 /**
  * Created by pavel on 12.02.18.
  */
 
 public class MainActivity extends AppCompatActivity {
-
-    ChartData[] mChartData;
 
 
     @Override
@@ -37,42 +23,9 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.tv_ticker);
 
         Button button = findViewById(R.id.btn_go);
-        OkHttpClient client = new OkHttpClient();
-        long todayUTC = new Date().getTime()/1000;
-        Request request = new Request.Builder().url(
-                Util.buildUrl(Util.CurrencyPair.USDT_BTC,
-                        todayUTC - 60*60*24*5,
-                        todayUTC,
-                        Util.Period.TWO_HOURS
-                        )).build();
-        GsonBuilder builder = new GsonBuilder();
-        Gson gson = builder.create();
         button.setOnClickListener(v -> {
-            Thread thread = new Thread() {
-                @Override
-                public void run() {
-                    try (Response response = client.newCall(request).execute()) {
-                        mChartData = gson.fromJson(response.body().string(), ChartData[].class);
-                        // Ограничение на количество запросов - не более 5 в секунду
-                        Thread.sleep(200);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    } catch (NullPointerException e) {
-                        e.printStackTrace();
-                    }
-
-                    // Отображение полученных данных о котировках в TextView
-                    String editable = "";
-                    DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy hh:mm", Locale.getDefault());
-                    for (ChartData entry: mChartData) editable = editable + dateFormat.format(new java.util.Date(entry.getDate()*1000)) + " : " + entry.getClose() + "\n";
-                    final String textToDisplay = editable;
-                    textView.post(() -> textView.setText(textToDisplay));
-                }
-            };
-            thread.setPriority(Thread.MIN_PRIORITY);
-            thread.start();
+            Intent intent = new Intent(MainActivity.this, EngineService.class);
+            startService(intent);
         });
     }
 }
